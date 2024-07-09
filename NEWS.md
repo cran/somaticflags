4209aa03df80f1b79f3f6c8184b5f38345ee17d8
Sam El-Kamand <selkamand@ccia.org.au>
2024-07-09 15:50:06 +0000

    version 0.1.0

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# somaticflags 0.1.0
+
+* Initial CRAN submission.
---
+++ R/somaticflags.R
@@ -0,0 +1,15 @@
+read_somaticflags <- function(){
+  path = system.file(package="somaticflags", "extdata/somaticflags.csv")
+  utils::read.csv(path, header = TRUE, sep = ",")
+}
+
+update_data_objects <- function(){
+  if(!requireNamespace("devtools", quietly = TRUE)) stop("Please install `devtools` before running `update_data_objects()`")
+  if(!requireNamespace("usethis", quietly = TRUE)) stop("Please install `usethis` before running `update_data_objects()`")
+
+  somaticflags_df <- read_somaticflags()
+  somaticflags <- somaticflags_df[["Gene"]]
+  usethis::use_data(somaticflags_df, overwrite = TRUE)
+  usethis::use_data(somaticflags, overwrite = TRUE)
+  devtools::document()
+}
